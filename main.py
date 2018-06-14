279e94b3c83b6378d8292bf026c61b285b9ca613
Banerjee786 <priyambanerjee08@gmail.com>
2018-06-14 11:14:25 -0500

    Second commit

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ app = Flask(__name__)
 def my_form():
     return render_template('my-form.html')
 
+'''
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    if request.method== 'POST':
+        range1 = request.form['text1']
+        range2 = request.form['text2']
+'''
+
 
 
 if __name__ == '__main__':
