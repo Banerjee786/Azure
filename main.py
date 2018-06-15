c3c2a9753d106eaa77670a3d07372852d25685ae
Banerjee786 <priyambanerjee08@gmail.com>
2018-06-15 02:04:01 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # from flask import Flask
 from flask import Flask, request, render_template
-import pyodbc
+#import pyodbc
 import time
 
 
 app = Flask(__name__)
-
+'''
 server = 'banerjee.database.windows.net'
 database = 'banerjeedb'
 username = 'Priyam360'
 password = 'Priyam555!'
 driver = '{ODBC Driver 13 for SQL Server}'
+'''
 
 @app.route('/')
 def display():
+    '''
     conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     cursor = conn.cursor()
     start = time.time()
@@ -21,7 +23,9 @@ def display():
     rows = cursor.fetchall()
     end = time.time()
     executiontime = end - start
-    return render_template('searchearth.html', rows=rows, executiontime=executiontime)
+    '''
+    return 'Hi Priyam'
+    #return render_template('searchearth.html', rows=rows, executiontime=executiontime)
 
 if __name__ == '__main__':
   app.run()
