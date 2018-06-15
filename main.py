adbe046e36a50a63c6fb9e9ac859f0ef3cae6132
Banerjee786 <priyambanerjee08@gmail.com>
2018-06-15 02:07:01 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 # from flask import Flask
 from flask import Flask, request, render_template
-#import pyodbc
+import pypyodbc
 import time
 
 
 app = Flask(__name__)
-'''
+
 server = 'banerjee.database.windows.net'
 database = 'banerjeedb'
 username = 'Priyam360'
 password = 'Priyam555!'
 driver = '{ODBC Driver 13 for SQL Server}'
-'''
+
 
 @app.route('/')
 def display():
-    '''
-    conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    conn = pypyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     cursor = conn.cursor()
     start = time.time()
     cursor.execute("SELECT top 10 * FROM equake where nst = 24")
     rows = cursor.fetchall()
     end = time.time()
     executiontime = end - start
-    '''
-    return 'Hi Priyam'
-    #return render_template('searchearth.html', rows=rows, executiontime=executiontime)
+    return render_template('searchearth.html', rows=rows, executiontime=executiontime)
 
 if __name__ == '__main__':
   app.run()
