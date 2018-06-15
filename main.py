d921a210ca58734ab586d0a0ccabd4f045e6d0b1
Banerjee786 <priyambanerjee08@gmail.com>
2018-06-15 16:17:25 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # from flask import Flask
 from flask import Flask, request, render_template
-import pyodbc
+import pypyodbc
 import time
 
 
@@ -15,7 +15,7 @@ driver = '{ODBC Driver 13 for SQL Server}'
 
 @app.route('/')
 def display():
-    conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    conn = pypyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     cursor = conn.cursor()
     start = time.time()
     cursor.execute("SELECT top 10 * FROM [equake] where nst = 24")
